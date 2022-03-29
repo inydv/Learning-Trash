22a5e9fcfb6d9f9edb6119c89e84c224f932a63d
Lokesh Yadav <i_nydv@hotmail.com>
2022-03-29 10:49:46 +0530

    Add 'react-stripe-checkout'

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../Styles/Cart.css";
+import StripeCheckout from "react-stripe-checkout";
 
 function Cart() {
+  const KEY =
+    "pk_test_51KiFjwSAuqXbyMkWgId8oftRCtEzWBU3CSrIOP9L34PvtVAUlqvPUrclGqJHHkq8Zmnbf4zn3vQfgOBOUBLL56bt004YUEROEP";
+  const [stripeToken, setStripeToken] = useState(null);
+
+  const ontoken = (token) => {
+    setStripeToken(token);
+  };
+
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        const res = await axios.post("http://localhost:5000/checkout/payment", {
+          tokenId: stripeToken.id,
+          amount: 500,
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    stripeToken && makeRequest  // call makeRequest
+  }, [stripeToken]);
+
   return (
     <div className="cart">
       <div className="container">
@@ -59,13 +82,22 @@ function Cart() {
         <div className="bottom">
           <div className="total">
             <p className="subTotal">SUBTOTAL</p>
-            <span className="totalSpan">
-              500
-            </span>
+            <span className="totalSpan">500</span>
           </div>
-          <div className="button">
-            <button className="btn">CHECKOUT</button>
-          </div>
+          <StripeCheckout
+            name="THE LITTLE THINGS"
+            // image=""
+            // description=""
+            billingAddress
+            shippingAddress
+            amount={500}
+            token={ontoken}
+            stripeKey={KEY}
+          >
+            <div className="button">
+              <button className="btn">CHECKOUT</button>
+            </div>
+          </StripeCheckout>
         </div>
       </div>
     </div>
