697a8a1edefc3ad8649ce2030b3ccc41e8b9df23
inydv <i_nydv@hotmail.com>
2022-08-07 17:06:27 +0530

    shift to redux-thunk

--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store";
-import { PersistGate } from "redux-persist/integration/react";
+import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
         <App />
-      </PersistGate>
     </Provider>
   </BrowserRouter>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,30 +1,13 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { configureStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import userRedux from "./user/userRedux";
 import newsLetterRedux from "./newsLetter/newsLetterRedux";
 import productsRedux from "./product/productsRedux";
 import cartRedux from "./cart/cartRedux";
 import newOrderRedux from "./order/newOrderRedux";
 
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-
-import storage from "redux-persist/lib/storage";
-
-const persistConfig = {
-  key: "root",
-  version: 1,
-  storage,
-};
-
-const rootReducer = combineReducers({
+const reducer = combineReducers({
   user: userRedux,
   newsLetter: newsLetterRedux,
   products: productsRedux,
@@ -32,16 +15,72 @@ const rootReducer = combineReducers({
   newOrder: newOrderRedux
 });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+let initialState = {
+  cart: {
+    cartItems: localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : [],
+    shippingInfo: localStorage.getItem("shippingInfo")
+      ? JSON.parse(localStorage.getItem("shippingInfo"))
+      : {},
+  },
+};
 
-export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-});
+const middleware = [thunk];
 
-export let persistor = persistStore(store);
+const store = configureStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
+
+// Redux Persist  --- redux-persist --- use local storage === redux-thunk use state memory
+// import { configureStore, combineReducers } from "@reduxjs/toolkit";
+// import userRedux from "./user/userRedux";
+// import newsLetterRedux from "./newsLetter/newsLetterRedux";
+// import productsRedux from "./product/productsRedux";
+// import cartRedux from "./cart/cartRedux";
+// import newOrderRedux from "./order/newOrderRedux";
+
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from "redux-persist";
+
+// import storage from "redux-persist/lib/storage";
+
+// const persistConfig = {
+//   key: "root",
+//   version: 1,
+//   storage,
+// };
+
+// const rootReducer = combineReducers({
+//   user: userRedux,
+//   newsLetter: newsLetterRedux,
+//   products: productsRedux,
+//   cart: cartRedux,
+//   newOrder: newOrderRedux
+// });
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// export const store = configureStore({
+//   reducer: persistedReducer,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
+// });
+
+// export let persistor = persistStore(store);
